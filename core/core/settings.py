35e03295e3b723f3a89db178fa209eed1680dc50
alimushali <iakochenkov@gmail.com>
2023-05-23 09:36:07 +0300

    adjusted .gitignore

--- core/core/settings.py
+++ core/core/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '..' / 'db.sqlite3',
     }
 }
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC = BASE_DIR / 'static'
+STATIC = BASE_DIR / '..' / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
