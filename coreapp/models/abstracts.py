50f7db69957b09311778cf3d9d210002dec311f5
alimushali <iakochenkov@gmail.com>
2023-05-23 09:48:00 +0300

    added abstract models

---
+++ coreapp/models/abstracts.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class AbstractUnit1(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    numeric_property = models.IntegerField()
+
+
+    class Meta:
+        table_name="abstract_unit_1"
+
+
+class LinkedUnit1(models.Model):
+    abstract_unit_id = models.ForeignKey(
+        AbstractUnit1,
+        primary_key=True
+    )
+    name = models.TextField()
+    description = models.TextField()
+    numeric_property = models.IntegerField()
+
+    class Meta:
+        table_name="linked_unit_1"
+
